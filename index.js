3b90b87127a6cc8b5a50730b1f882dae9672bb1f
Boedi01Gaming <budiasmoking69@gmail.com>
2025-04-18 19:54:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client } = require("discord.js-selfbot-v13");
+const express = require("express");
+const app = express();
+const client = new Client();
+
+const status = {
+  name: "Main game bareng",
+  type: 0,
+  url: null,
+};
+
+client.on("ready", () => {
+  console.log(`${client.user.username} sudah online sebagai selfbot!`);
+  client.user.setPresence({
+    activities: [{
+      name: status.name,
+      type: status.type,
+      url: status.url || null,
+    }],
+    status: "online",
+  });
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+app.get("/", (req, res) => res.send("Selfbot is running!"));
+app.listen(3000, () => console.log("Keep-alive server running"));
